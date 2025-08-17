82ef9ff0f9ac51f9a2a3934c587d1d27d5b9a62c
Andre Fellows <fellows.devel@gmail.com>
2025-08-17 20:23:14 -0300

    add test

--- src/test/java/com/fellows/tax_calculator/TaxCalculatorApplicationTests.java
+++ src/test/java/com/fellows/tax_calculator/TaxCalculatorApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fellows.tax_calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaxCalculatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  TaxCalculatorApplication application;
+
+  @Test
+  void contextLoads() {
+    Assertions.assertDoesNotThrow(() -> TaxCalculatorApplication.main(new String[]{}));
+  }
 
 }
